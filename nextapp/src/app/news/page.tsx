2b731e5cf3e6fce789d59c88a3bfd66aa0c48031
AKASH-GPT-1678 <akashgpt5974@gmail.com>
2025-08-29 17:51:17 +0530

    added payment api

--- nextapp/src/app/news/page.tsx
+++ nextapp/src/app/news/page.tsx
@@ -1,6 +1,23 @@
+"use client"
 import React from 'react'
 import axios from 'axios';
+import { set } from 'zod';
+// types.ts
+export interface TopRead {
+    id: string;
+    title: string;
+    content: string;
+    category: string;
+    source: string;
+    url: string;
+    imageUrl: string;
+    reads: number;
+    publishedAt: string;
+    createdAt: string;
+    updatedAt: string;
+}
 const NewsPage = () => {
+    const [newsData, setNewsData] = React.useState<TopRead[]>([]);
     const endpoint = "https://polytikka-production.up.railway.app/api/allnews";
 
     // Function to fetch data
@@ -8,6 +25,8 @@ const NewsPage = () => {
         try {
             const response = await axios.get(endpoint);
             // API se aaya data return kar
+            console.log(response.data);
+            setNewsData(response.data.data);
             return response.data;
         } catch (error) {
             console.error("Error fetching top reads:", error);
@@ -20,7 +39,25 @@ const NewsPage = () => {
         getTopReads();
     }, []);
     return (
-        <div>
+        <div className='flex flex-row flex-wrap gap-4 p-8'>
+            {
+                newsData.length > 0 && (
+                    newsData.map((news: TopRead, index) => (
+                        <div key={index} className='flex flex-col max-w-[300px] rounded-2xl shadow-xl p-2 ' onClick={()=>window.open(news.url)}>
+                            <img src={news.imageUrl} alt={news.title} />
+                            <h1>{news.title}</h1>
+                            <p>{news.content.substring(0, 100)}</p>
+                            <p>{news.category}</p>
+                            <p>{news.source}</p>
+                      
+                         
+                            <p>{news.publishedAt}</p>
+                           
+                        </div>
+                    ))
+                )
+            }
+
 
         </div>
     )
