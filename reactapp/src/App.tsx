3d9a32aaf752b6d6042a00651e7776d17490b22d
AKASH-GPT-1678 <akashgpt5974@gmail.com>
2025-08-30 12:50:22 +0530

    local storage

--- reactapp/src/App.tsx
+++ reactapp/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Taskpage from './Components/Taskpage';
 import NewsPage from './Components/Newspage';
+import Login from './Components/Login';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Taskpage />} />
           <Route path='/news' element={<NewsPage/>}/>
+          <Route path='/login' element={<Login/>}/>
         </Routes>
       </BrowserRouter>
     </div>
